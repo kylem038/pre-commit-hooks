0b3d0e8c03d124b98bddfa0ba96831c85d8c218f
Brian Schiller <bgschiller@gmail.com>
2016-03-17 16:57:49 -0500

    add migration rename suggestion

--- pre_commit_hooks/django_migration_merge.py
+++ pre_commit_hooks/django_migration_merge.py
@@ -1,5 +1,15 @@
+import random
 import subprocess
 import sys
+
+with open('animals', 'r') as f:
+    animals = [line.strip() for line in f]
+with open('adjectives', 'r') as f:
+    adjectives = [line.strip() for line in f]
+
+
+def identifier():
+    return '{}_{}'.format(random.choice(adjectives), random.choice(animals))
 
 
 def get_env_python():
@@ -22,6 +32,8 @@ def check_for_django_migrations(argv=None):
         print 'Your migrations look like they could use a merge.'
         print 'psst, you can probably fix this by running'
         print '    ./manage.py makemigrations --merge'
+        print "    (give it a weird name so it doesn't conflict when somebody else does this.)"
+        print "Rather than 0012_merge.py, how about '0012_{}_merge.py'?".format(identifier())
         return 1
 
 if __name__ == '__main__':
